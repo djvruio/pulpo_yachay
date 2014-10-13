75d33e50e833f8367d30f1c651ae4d964eefd0fb
Darwin <djvruio@gmail.com>
2014-10-13 10:35:56 -0500

    pulpo_yachay: projects for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @projects = @user.projects
     unless current_user.admin?
       unless @user == current_user
         redirect_to :back, :alert => "Access denied."
