2fba6c2b6919f07638a7f4fe8b7874658dad1675
Giovanni <giovanni.moreno.aguilar@gmail.com>
2014-10-15 15:03:20 -0500

    pulpo_yachay:Vistas_Proyectos_Tareas_V2

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -3,13 +3,13 @@ module TasksHelper
 	def format_task_status(task)
 		case task.status
 		when 'open'
-  			content_tag(:span, 'open', :class => "label pull-right label-success")
+  			content_tag(:span, 'open', :class => "label pull-right label-warning")
 		when 'close'  
   			content_tag(:span, 'close', :class => "label pull-right label-info")
 		when 'delayed'
   			content_tag(:span, 'delayed', :class => "label pull-right label-danger")
   		else
-  			content_tag(:span, 'in_progress', :class => "label pull-right label-warning")
+  			content_tag(:span, 'in_progress', :class => "label pull-right label-progress")
 		end
 	end
 
@@ -21,6 +21,18 @@ module TasksHelper
   			content_tag(:span, 'medium', :class => "label pull-right label-info") 
   		else
   			content_tag(:span, 'high', :class => "label pull-right label-warning") 
+		end
+	end
+
+	def flag_task_deadline(task)
+		date_actual=DateTime.now.to_date.to_s
+		date_dl_task=task.deadline.to_date.to_s
+#		date_created_task=task.created_at.to_date.to_s
+
+		if (date_dl_task >= date_actual)
+			content_tag(:span, '',:class=>"glyphicon glyphicon-flag in-progress")
+		else
+			content_tag(:span, '', :class => "glyphicon glyphicon-flag delayed")
 		end
 	end
 	
