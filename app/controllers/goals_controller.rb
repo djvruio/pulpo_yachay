796f11a5f8bc58cadd4d46f7f69639c3f91801c2
Darwin <djvruio@gmail.com>
2014-10-22 10:59:22 -0500

    pulpo_yachay: goals modified

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,15 +1,6 @@
 class GoalsController < ApplicationController
   def create
-<<<<<<< HEAD
-    binding.pry
-    @goal = current_user.goals.build(goals_params)
-    if @goal.save
-      flash[:success] = "Objetivo creado!"
-      redirect_to root_url
-    else
-      render 'visitors/index'
-=======
-    #binding.pry
+
     @goal = Goal.new(goals_params)
     respond_to do |format|
       if @goal.save
@@ -19,16 +10,11 @@ class GoalsController < ApplicationController
         format.html { render action: "new" }
         format.json { render json: @goal.errors, status: :unprocessable_entity }
       end
->>>>>>> MyArm
     end
   end
 
   def index
-<<<<<<< HEAD
-    @goals_index=Goal.all
-=======
     @goals = Goal.all
->>>>>>> MyArm
   end
 
   def new
