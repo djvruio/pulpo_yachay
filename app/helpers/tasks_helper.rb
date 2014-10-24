c5475d0234b547e12865883b0b37e72f23c116a6
fabohack <fabianrjimenez@gmail.com>
2014-10-24 14:06:23 -0500

    yachay_pulpo:Allow_Enter_a_new_task_undated

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -23,19 +23,19 @@ module TasksHelper
   			content_tag(:span, 'high', :class => "label pull-right label-warning") 
 		end
 	end
-
-	def flag_task_deadline(task)
-		date_actual=DateTime.now.to_date.to_s
-		date_dl_task=task.deadline.to_date.to_s
-#		date_created_task=task.created_at.to_date.to_s
-
-		if (date_dl_task >= date_actual)
-			content_tag(:span, '',:class=>"glyphicon glyphicon-flag in-progress")
-		else
-			content_tag(:span, '', :class => "glyphicon glyphicon-flag delayed")
-		end
-	end
-
+  def flag_task_deadline(task)
+    if task.deadline.present?
+        date_actual=DateTime.now.to_date.to_s
+        date_dl_task=task.deadline.to_date.to_s
+        if (date_dl_task >= date_actual)
+          content_tag(:span, '',:class=>"glyphicon glyphicon-flag in-progress")
+        else
+          content_tag(:span, '', :class => "glyphicon glyphicon-flag delayed")
+        end
+    else
+      content_tag(:span, '',:class=>"glyphicon glyphicon-flag No Set Yet")
+    end
+  end
 	def is_new_task(action)
 		case action
 		when 'new'
