cbae5d5a75f74858d684e8b01f4a30e374a1ff10
Darwin <djvruio@gmail.com>
2014-10-27 18:53:07 -0500

    pulpo_yachay: revert state to heroku

--- db/migrate/20141023001123_create_states.rb
+++ db/migrate/20141023001123_create_states.rb
@@ -1,9 +1,9 @@
 class CreateStates < ActiveRecord::Migration
-  # def change
-  #   create_table :states do |t|
-  #     t.string :name
-  #     t.integer :value
-  #     t.timestamps
-  #   end
-  # end
+  def change
+    create_table :states do |t|
+      t.string :name
+      t.integer :value
+      t.timestamps
+    end
+  end
 end
