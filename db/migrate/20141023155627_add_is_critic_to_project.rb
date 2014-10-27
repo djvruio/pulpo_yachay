37cd96275013d90fc29fcaf26be2c5eda406ab2b
Giovanni <giorelar@yahoo.es>
2014-10-27 16:30:34 -0500

    pulpo_yachay:Eliminacion de columna is_critic para la migracion

--- db/migrate/20141023155627_add_is_critic_to_project.rb
+++ db/migrate/20141023155627_add_is_critic_to_project.rb
@@ -1,6 +1,5 @@
 class AddIsCriticToProject < ActiveRecord::Migration
   def change
-  	remove_column :projects, :is_critic,  :boolean, default: false
     add_column :projects, :is_critical,  :boolean, default: false
   end
 end
