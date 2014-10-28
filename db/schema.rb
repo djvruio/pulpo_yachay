eb05020707704be159e353265a1646d49e452dfe
Darwin <djvruio@gmail.com>
2014-10-28 10:34:27 -0500

    pulpo_yachay: migracion Add State To Tasks.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024143302) do
+ActiveRecord::Schema.define(version: 20141028152942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,12 +81,12 @@ ActiveRecord::Schema.define(version: 20141024143302) do
     t.datetime "deadline"
     t.integer  "complexity",     default: 0, null: false
     t.integer  "score",          default: 0, null: false
-    t.integer  "status",         default: 0, null: false
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "assigned_to_id"
+    t.integer  "state_id"
   end
 
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
