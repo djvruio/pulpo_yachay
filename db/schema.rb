a108212449e558ea3be54cd072a86c00e73d2522
Darwin <djvruio@gmail.com>
2014-10-29 15:57:12 -0500

    pulpo_yachay: cambios a tabla projects aplicado.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028152942) do
+ActiveRecord::Schema.define(version: 20141029202017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,12 +57,13 @@ ActiveRecord::Schema.define(version: 20141028152942) do
   add_index "organizations", ["ancestry"], name: "index_organizations_on_ancestry", using: :btree
 
   create_table "projects", force: true do |t|
-    t.string   "name"
+    t.text     "name"
     t.text     "description"
     t.boolean  "is_strategic",           default: false, null: false
     t.integer  "status",                 default: 0,     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "is_critic",              default: false
     t.boolean  "is_critical",            default: false
     t.integer  "price_cents",  limit: 8, default: 0
     t.string   "alias"
@@ -70,9 +71,9 @@ ActiveRecord::Schema.define(version: 20141028152942) do
 
   create_table "states", force: true do |t|
     t.string   "name"
+    t.integer  "value"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "value"
   end
 
   create_table "tasks", force: true do |t|
